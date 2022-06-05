2df79fc5d23f9bfd106e09efc083577593474479
Tomas de Priede <tomasdepriede@gmail.com>
2022-06-05 18:02:37 -0300

    removed implementation

--- my-app/src/features/cards/card.component.tsx
+++ my-app/src/features/cards/card.component.tsx
@@ -7,10 +7,7 @@ export type CardProps = {
 
 const Card:FC<CardProps> = ({character}: CardProps) => {
    return <div>
-        <h3>{character.name}</h3>
-       { character.image && <img src={character.name} alt={character.name}/>}
-       { character.followers > 0 && <div>{character.followers} followers</div>}
-       { character.followers === 0 && <div>No followers yet</div>}
+
     </div>
 }
 
--- my-app/src/features/hooks/useAlert.hook.tsx
+++ my-app/src/features/hooks/useAlert.hook.tsx
@@ -2,11 +2,9 @@
 // Podemos cambiar la implementacion a nuestra propia libreria de alerts, gracias al principio solid de inversion de dependencia
 const useAlert = () => {
     const dispatchSuccessAlert = (message: string) => {
-        alert('SUCCESS: ' + message);
     };
 
     const dispatchErrorAlert = (message: string) => {
-        alert('ERROR: ' + message);
     };
 
     return {
