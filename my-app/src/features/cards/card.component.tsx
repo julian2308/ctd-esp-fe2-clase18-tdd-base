a9cca01a4a3c5aaa5051f35fb6559ccd24046773
julian2308 <julian.d.alvarado23@gmail.com>
2022-07-03 16:29:49 -0500

    All test are done and are working at all

--- my-app/src/features/cards/card.component.tsx
+++ my-app/src/features/cards/card.component.tsx
@@ -6,9 +6,13 @@ export type CardProps = {
 }
 
 const Card:FC<CardProps> = ({character}: CardProps) => {
-   return <div>
+   return (
 
-    </div>
+    <div>
+        <p>{character.name}</p>
+        <img src={character.image ? character.image : undefined} alt={character.image ? character.name : "Doesn't have alt"}  />
+        <p>{character.followers > 0 ? `${character.followers} followers` : "No followers yet"}</p>
+    </div>)
 }
 
 export default Card;
--- my-app/src/features/hooks/useAlert.hook.tsx
+++ my-app/src/features/hooks/useAlert.hook.tsx
@@ -2,9 +2,14 @@
 // Podemos cambiar la implementacion a nuestra propia libreria de alerts, gracias al principio solid de inversion de dependencia
 const useAlert = () => {
     const dispatchSuccessAlert = (message: string) => {
+        window.onload
+        alert(`SUCCESS: ${message}`)
     };
 
     const dispatchErrorAlert = (message: string) => {
+        window.onload
+        alert(`ERROR: ${message}`)
+
     };
 
     return {
